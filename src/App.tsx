895f961a4c59ae3085bb94d2b183d02a7f943507
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-08-17 16:36:30 +0300

    feat: add header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Header from "./components/Header/Header";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="app">
+      <Header />
     </div>
   );
 }
